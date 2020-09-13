da1dc72f5ebe3403aafa0abbba6ddbcda8c34d23
abhishek <abhisheknawani98@gmail.com>
2020-09-13 15:15:19 +0530

    new commit

---
+++ number_of_times_sorted_array_is_rotated.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+int rotated(int a[],int s,int e,int n)
+{
+	int pivot;
+	while(s<=e)
+	{
+		int mid = s+(e-s)/2;
+		int next = (mid+1)%n;
+		int prev = (mid+n-1)%n;
+		if(a[mid]<a[prev] && a[mid]<a[next])
+		{
+			pivot=mid;
+			break;
+		}
+		else if(a[s]<=a[mid])
+		{
+			s=mid+1;
+		}
+		else
+		{
+			e=mid-1;
+		}
+	}
+	return pivot;
+}
+
+int main() 
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+    int n,x;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    if(a[0]<a[n-1] || n==1)
+    {
+    	x=0;
+    	cout<<x;
+    }
+    else
+    {
+    	x=rotated(a,0,n-1,n);
+    	cout<<(n-x);
+    }
+    return 0;	
+ } 
